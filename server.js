59deb147243363c8105f0afb8a5b155a36e40193
Jani Eväkallio <jani.evakallio@gmail.com>
2017-02-11 22:13:00 +0000

    Add project template

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const fetch = require('isomorphic-fetch');
+const path = require('path');
+const app = module.exports = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// add your routes here :)
+
+app.listen(4000);
