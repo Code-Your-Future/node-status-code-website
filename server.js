c8f23cb51e12e1304d549af54fe23869ed08d44b
Amit Shah <amit@amwam.me>
2017-02-12 12:31:48 +0000

    Force eslint error

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const fetch = require('isomorphic-fetch');
-const path = require('path');
+const express = require("express");
+const fetch = require("isomorphic-fetch");
+const path = require("path");
 const app = module.exports = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
 
 // add your routes here :)
 
