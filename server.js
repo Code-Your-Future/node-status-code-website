386fb5fea522bbf66b96e7e96c1486f4eedbecab
sentayhuJo <sentayhu.alim@mail.com>
2017-02-13 22:24:19 +0000

    root end point created, call api from localhost:3000/code, get the result and desplay on localhost:4000/

--- server.js
+++ server.js
@@ -4,13 +4,38 @@ const path = require('path');
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-// add your routes here :)
+app.get('/:code', (req, res) => {
+  const code = req.params.code;
+  const promise1 = fetch('http://localhost:3000/code/' + code)
+  .then(response => response.json());
+  const promise2 = fetch('http://numbersapi.com/' + code)
+  .then(response => response.text());
 
+  Promise.all([promise1, promise2])
+  .then((arr) => {
+    res.render('code', { 
+      'code': arr[0].code, 
+      'phrase': arr[0].phrase,
+      'description': arr[0].description, 
+      'fact': arr[1] 
+    });
+  })
+  .catch(() => {
+    res.send('Invalid Query, Page not found !');
+  });
+});
+
+app.get('/', (req, res) => {
+  fetch('http://localhost:3000/code')
+  .then(response => response.json())
+  .then((allCode) => {
+    res.send(allCode);
+  });
+});
 app.listen(4000);
 
 module.exports = app;
