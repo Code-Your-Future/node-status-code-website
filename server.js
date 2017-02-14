f62442aeb53e321923e8c5c8f92dcfa1057dbade
ehmed-kino <ehmedaff@gmail.com>
2017-02-14 14:06:51 +0000

    solve tester problem

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fetch = require('isomorphic-fetch');
 const path = require('path');
+const request = require('request');
 
 const app = express();
 
@@ -10,6 +11,19 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add your routes here :)
+app.get('/:code', (req, res) => {
+
+  const { code } = req.params;
+
+  request(`http://localhost:8080/code/${code}`, (error, response, body)=>{
+    console.log('requested', {error, body});
+    if(!error && response.statusCode === 200) {
+      const parsed = JSON.parse(body);
+      if(parsed === {}) {return}
+      return res.render('code', parsed);
+    }
+  })
+  });
 
 app.listen(4000);
 
