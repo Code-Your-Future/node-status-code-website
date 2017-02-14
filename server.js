781e4563b7f26b5e45694ec0fc54e7fe85c69326
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-02-14 13:28:14 +0000

    express route that accepts a status code as a parameter

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add your routes here :)
+app.get('/:code', function (req,res) {
+	const url = 'http://localhost:8080/code/' + req.params.code;
+	const url2 = 'http://numbersapi.com/' + req.params.code;
+	const promise1 = fetch(url).then((res) => res.json());
+	const promise2 = fetch(url2).then((res) => res.text());
+	Promise.all([promise1,promise2])
+	.then(function ([json,text]) {
+		res.render('code', {code:json})
+	})
+	.catch(function (err) {
+		console.error(err)
+	});
+});
 
 app.listen(4000);
 
