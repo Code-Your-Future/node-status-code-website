cb8c08c9f6abe454b9a00e8a193bb97c539b4242
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-02-18 11:26:20 +0000

    new template index and new website route /

--- server.js
+++ server.js
@@ -10,20 +10,38 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add your routes here :)
-app.get('/:code', function (req,res) {
-	const url = 'http://localhost:8080/code/' + req.params.code;
-	const url2 = 'http://numbersapi.com/' + req.params.code;
-	const promise1 = fetch(url).then((res) => res.json());
-	const promise2 = fetch(url2).then((res) => res.text());
-	Promise.all([promise1,promise2])
-	.then(function ([json,text]) {
-		res.render('code', {code:json, text:text})
-	})
-	.catch(function (err) {
-		console.error(err)
-	});
+
+app.get('/:code', (req, res) => {
+  const url = `http://localhost:8080/code/${req.params.code}`;
+  const url2 = `http://numbersapi.com/${req.params.code}`;
+  const promise1 = fetch(url).then(resp => resp.json());
+  const promise2 = fetch(url2).then(resp => resp.text());
+  Promise.all([promise1, promise2])
+  .then(([json, txt]) => {
+    res.render('code', { code: json, text: txt });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+});
+
+app.get('/', (req, res) => {
+  const url = 'http://localhost:8080/code';
+  fetch(url, {
+    method: 'GET',
+  })
+  .then((apiRes) => {
+    apiRes.json()
+      .then((json) => {
+        res.render('index', { codes: json });
+      });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 });
 
 app.listen(4000);
 
 module.exports = app;
+
