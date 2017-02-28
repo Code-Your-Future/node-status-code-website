d917535d7447897758d0e63dc5c09f26b6270afb
mohibhb <mohib@InstallmacOSSierra10121-a820662a6550.local>
2017-02-28 20:41:57 +0000

    website work

--- server.js
+++ server.js
@@ -10,7 +10,49 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add your routes here :)
+app.get('/:code', (req,res)=>{
+	const status = req.params.code;
+
+    const p1 = `http://localhost:8080/code/${status}`;
+    fetch(p1)
+    .then(function(response) {
+    	console.log(response);
+      return response.json();
+
+    })
+    .then(function(response){
+    	res.render('code',response);
+    })
+
+    .catch(function(){
+    	console.log("Rejected promise")
+    })
+
+	
+});
+
+app.get('/', (req,res)=>{
+	
+    const p1 = `http://localhost:8080/code`;
+    fetch(p1)
+    .then(function(response) {
+    	console.log(response);
+      return response.json();
+
+    })
+    .then(function(response){
+    	res.render('index',{'content':response});
+    })
+
+    .catch(function(){
+    	console.log("Rejected promise")
+    })
+
+	
+});
 
 app.listen(4000);
 
 module.exports = app;
+
+
